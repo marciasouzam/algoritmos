ad48b7d761b42cf202c728d8552defd0558335d2
marciasouzam <156952820+marciasouzam@users.noreply.github.com>
2025-10-08 11:30:53 +0000

    Atividade com um erro a ser corrigido

--- codigos/1_bimestre/maiorElemento/maiorElemento.cpp
+++ codigos/1_bimestre/maiorElemento/maiorElemento.cpp
@@ -21,6 +21,17 @@ int maior_array2(int a[], int tamanho) {
     return maior;
 }
 
+ int maior_array3(int a[], int tamanho) {
+    int maior = a[0];
+    for (int i = 1; i < tamanho; ++i) {
+        if (i == 20) 
+            a[i] = 100; 
+        if (a[i] > maior) {
+            maior = a[i]; } 
+        }
+    return maior; 
+} 
+
 
 int main() {
     int n;
@@ -30,11 +41,12 @@ int main() {
         std::cin >> a[i];
     int maior = maior_array(a, n);
     int maior2 = maior_array2(a, n);
-    std::cout << maior << std::endl;
-    std::cout << maior2 << std::endl;
+    int maior3 = maior_array3(a, n);
+    std::cout  << "Maior valor da funcao maior_array: "<< maior << std::endl;
+    std::cout  << "Maior valor da funcao maior_array2: " << maior2 << std::endl;
+    std::cout << "Maior valor da funcao maior_array3: " << maior3 << std::endl;
 
     for (int i = 0; i < n; ++i) 
         std::cout << a[i] << std::endl;
-    
-    return 0;
+       return 0;
 }
